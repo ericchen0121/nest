8001ab79b0a1a055599a3209e8ed4df3a7ba9d3e
Matthew Harwood <matthhar12@gmail.com>
2015-03-12 19:25:32 -0700

    scrips

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    //selects the video
+    var vid = $('.video')[0];
+    var pause = $('.pause');
+    var play = $('.play');
+
+
+    //video movement function
+
+    //pause video
+    var pauseVideo = function() {
+      vid.pause();  
+    };
+
+    //play video
+    var playVideo = function() {
+      vid.play();
+    };
+    //.video movement function
+
+    // UI click events
+    pause.on( "click", pauseVideo );
+    play.on( "click", playVideo );
+
 });
