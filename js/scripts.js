e2cf90b2ae72d0dd2707246a50e5d5eaa6cda718
Matthew Harwood <matthhar12@gmail.com>
2015-03-12 23:49:04 -0700

    added the clickevent for hud

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,49 @@
 $( document ).ready(function() {
     //selects the video
-    var vid = $('.video')[0];
-    var pause = $('.pause');
-    var play = $('.play');
+    // var vid = $('.video')[0];
+    // var pause = $('.pause');
+    // var play = $('.play');
     var time = 0;
 
     //video movement function
 
     //pause video
-    var pauseVideo = function() {
-      vid.pause();  
+    // var pauseVideo = function() {
+    //   vid.pause();  
+    // };
+
+    // //play video
+    // var playVideo = function() {
+    //   vid.play();
+    // };
+
+
+    // var sceneCounter = 0;
+    // var timestamps = [2, 5,20,30,40];
+
+    // var nextScene = function() {
+    //   sceneCounter++
+    //   playVideo();
+    //   console.log('hey')
+    // };
+
+
+    // playVideo();
+    // vid.addEventListener('timeupdate',function(event){
+    //   time = vid.currentTime;
+    //   if(time <= timestamps[sceneCounter]){
+    //     playVideo();
+    //   } else {
+    //     pauseVideo();
+    //   }
+    // },false);
+
+    var nextProduct = function(){
+      console.log('asdf');
     };
-
-    //play video
-    var playVideo = function() {
-      vid.play();
-    };
-
-
-    var sceneCounter = 0;
-    var timestamps = [2, 5,20,30,40];
-
-    var nextScene = function() {
-      sceneCounter++
-      playVideo();
-      console.log('hey')
-    };
-
-
-    playVideo();
-    vid.addEventListener('timeupdate',function(event){
-      time = vid.currentTime;
-      if(time <= timestamps[sceneCounter]){
-        playVideo();
-      } else {
-        pauseVideo();
-      }
-    },false);
-
+    console.log('hery');
      // UI click events
-    pause.on( "click", pauseVideo );
-    $('.video').on("click", nextScene);
+    // pause.on( "click", pauseVideo );
+    // $('.video').on("click", nextScene);
+    $('.hud').on("click", nextProduct);
 });
